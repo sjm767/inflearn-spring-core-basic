c092b2d1c47898548de8d5e0ccccc8958da45348
jaeshim <jaeshim@gmarket.com>
2023-03-08 21:37:07 +0900

    20230307

--- inflearn/src/main/java/com/jaeshim/inflearn/spring/core/basic/order/OrderServiceImpl.java
+++ inflearn/src/main/java/com/jaeshim/inflearn/spring/core/basic/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package com.jaeshim.inflearn.spring.core.basic.order;
 
 import com.jaeshim.inflearn.spring.core.basic.discount.DiscountPolicy;
 import com.jaeshim.inflearn.spring.core.basic.discount.FixDiscountPolicy;
+import com.jaeshim.inflearn.spring.core.basic.discount.RateDiscountPolicy;
 import com.jaeshim.inflearn.spring.core.basic.member.Member;
 import com.jaeshim.inflearn.spring.core.basic.member.MemberRepository;
 import com.jaeshim.inflearn.spring.core.basic.member.MemoryMemberRepository;
@@ -9,7 +10,8 @@ import com.jaeshim.inflearn.spring.core.basic.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
   private final MemberRepository memberRepository = new MemoryMemberRepository();
-  private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//  private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+  private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
 
   @Override
