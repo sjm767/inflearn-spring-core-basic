6b0b863814debc9444053674f8b9d9fdfcac2bfe
jaeshim <jaeshim@gmarket.com>
2023-03-15 00:39:57 +0900

    20230314

---
+++ inflearn/src/test/java/com/jaeshim/inflearn/spring/core/basic/singleton/SingletonService.java
@@ -0,0 +1,19 @@
+package com.jaeshim.inflearn.spring.core.basic.singleton;
+
+public class SingletonService {
+
+  private static final SingletonService instance = new SingletonService();
+
+  public static SingletonService getInstance() {
+    return instance;
+  }
+
+  private SingletonService() {
+
+  }
+
+  public void logic() {
+    System.out.println("싱글톤 객체 로직 호출");
+  }
+
+}
